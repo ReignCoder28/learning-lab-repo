205dd84624b6cfe5dc68e2f2a92264ad216b8399
Imani wilson <178332713+ReignCoder28@users.noreply.github.com>
2025-07-14 23:42:02 -0700

    not finished ;-;

---
+++ script.js
@@ -0,0 +1,30 @@
+// GLOBAL ARRAY
+
+var shoppingCart = 0;
+
+
+const products = {
+    "cereal":{
+        price: "4.99"
+        += shoppingCart
+    },
+    "milk":{
+        price: "2.99"
+        += shoppingCart
+    },
+    "candy":{
+        price: "6.99"
+        += shoppingCart
+    },
+    "Oranges":{
+        price: "5.99"
+        += shoppingCart
+    }
+
+}; 
+
+// FUNCTION
+function total (){
+    console.log(shoppingCart)
+}
+
